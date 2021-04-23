814ed5e0696f73ed5721668fe1df1b45dc5c886d
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-23 14:33:39 +0300

    update

--- snake.js
+++ snake.js
@@ -1,9 +1,15 @@
 export const SNAKE_SPEED = 2 //how many times per second the page refreshes
-const snakeBody = [{ x: 11, y:11}]
+const snakeBody = [{ x: 10, y:11},{ x: 11, y:11},{ x: 12, y:11}]
 
 export function update(){
     console.log("update")
-
+    for (let i = snakeBody.length - 2; i >= 0; i--){
+        snakeBody[i+1] = {...snakeBody[i]}
+    }
+    // snakeBody[0].x=snakeBody[0].x+1
+    // snakeBody[0].x=snakeBody[0]
+    snakeBody[0].x +=1
+    snakeBody[0].x +=0
 }
 
 export function draw(gameBoard){
