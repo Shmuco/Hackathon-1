49f633be8cc16694ec3803ae11914a2ed4cc2395
lilarouge <71719843+lilarouge@users.noreply.github.com>
2021-04-24 23:18:27 +0200

    Final

--- game.js
+++ game.js
@@ -1,12 +1,21 @@
-import { update as updateSnake ,draw as drawSnake, SNAKE_SPEED } from './snake.js'
+import { update as updateSnake ,draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
 
 import { update as updateFood, draw as drawFood} from './food.js'
 
+import {outsideGrid} from './grid.js'
+
 // funtion to refersh the page in order to "repaint"
 let lastRenderTime = 0
+let gameOver= false
 const gameBoard = document.getElementById("game-board")
 
 function main(currentTime){
+    if (gameOver){
+        if(confirm('You just lost, press ok to play again!')){
+       window.location= '/'
+    }
+    return
+}
     window.requestAnimationFrame(main)
     const secondeSinceLastRenderTime =(currentTime - lastRenderTime)/1000 // gives us a variable in secods of how long between each refresh
     if (secondeSinceLastRenderTime<1/SNAKE_SPEED) return // This says if its hasnt been longer than 0.5 a second restart the function
@@ -28,6 +37,7 @@ window.requestAnimationFrame(main)
 function update(){ // Used to update position and food etc
     updateSnake()
     updateFood()
+    checkDeath()
 }
 
 
@@ -37,4 +47,8 @@ function draw(){ // Used to draw the updated frame
     drawFood(gameBoard)
 
     
+}
+
+function checkDeath(){
+    gameOver= outsideGrid(getSnakeHead()) || snakeIntersection()
 }
--- snake.js
+++ snake.js
@@ -32,12 +32,21 @@ export function expandSnake(amount) {
     newSegments+= amount
 }
 
-export function onSnake(position){
-    return snakeBody.some(segment=>{
+export function onSnake(position, { ignoreHead =false } = {}){
+    return snakeBody.some((segment, index)=>{
+        if (ignoreHead && index === 0) return false
         return equalPostitons (segment,position)
     }
 
     )
+}
+
+export function getSnakeHead(){
+    return snakeBody[0]
+}
+
+export function snakeIntersection(){
+    return onSnake(snakeBody[0], {ignoreHead: true})
 }
 function equalPostitons (pos1,pos2){
    return  pos1.x===pos2.x && pos1.y ===pos2.y
