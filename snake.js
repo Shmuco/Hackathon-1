d5288ec177cbc70e0e4ff647dd6d879c60f4e839
Shmuel Cohen <shmuel@Shmuels-MacBook-Pro.local>
2021-04-25 07:47:48 +0300

    update

--- snake.js
+++ snake.js
@@ -1,7 +1,7 @@
 import { getInputDirection } from "./input.js"
 
 
-export const SNAKE_SPEED = 5 //how many times per second the page refreshes
+export const SNAKE_SPEED = 10 //how many times per second the page refreshes
 const snakeBody = [{ x: 11, y:11}]
 let newSegments=0
 
